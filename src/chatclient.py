b20da86ad6e402d3fbcf53fdb5284275c3dc6229
mahmoud isa <mahmoudahalim0@gmail.com>
2025-12-23 09:11:28 +0200

    feat: Implement Client tui

--- src/chatclient.py
+++ src/chatclient.py
@@ -1,7 +1,17 @@
 import socket
 import sys
-import time
+import threading
 from chatui import init_windows, read_command, print_message, end_windows
+
+
+s = socket.socket()
+
+
+def receive_message():
+    init_windows()
+    while True:
+        message = s.recv(512).decode()
+        print_message(message.rstrip("\n"))
 
 
 def main(argv: list[str]):
@@ -10,33 +20,18 @@ def main(argv: list[str]):
         return 1
 
     [user, url, port] = argv[1:]
-    s = socket.socket()
     s.connect((url, int(port)))
     print("Connected to Server")
-    s.sendall(f"Hello from {user}".encode())
-    time.sleep(3)
-    s.sendall(b"")
-    # s.shutdown(socket.SHUT_WR)  # Graceful shutdown     instead of close
-    s.close()
-    return 0
+
+    # recieving thread
+    t = threading.Thread(target=receive_message, daemon=True)
+    t.start()
+
+    # main thread loop
+    while True:
+        message = read_command()
+        s.sendall(message.encode())
 
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
-
-
-# def draw_ui():
-#     pass
-#
-#
-# init_windows()
-#
-# while True:
-#     try:
-#         command = read_command("Enter a thing> ")
-#     except:
-#         break
-#
-#     print_message(f">>> {command}")
-#     continue
-# end_windows()
